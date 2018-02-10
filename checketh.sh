f725eaf337267045ceda508d4d1cc06231c8454a
DanielMarius <unixmarius@gmail.com>
2018-02-10 15:03:54 +0200

    Add files via upload

--- checketh.sh
+++ checketh.sh
@@ -9,10 +9,10 @@
 # OS:        Armbian & Raspbian
 #
 # Copyright: Copyright (c) 2018 Daniel Marius Gligor <mossadmobile@gmail.com>
-#            
+#
 # Purpose:
 # Script checks to see if WiFi or Ethernet has a network IP and if not
-# restart WiFi or Eth
+# restart WiFi or Ethernet
 #
 # Uses a lock file which prevents the script from running more
 # than one at a time.  If lockfile is old, it removes it
@@ -20,12 +20,14 @@
 # Instructions:
 # o Install where you want to run it from like /usr/local/bin
 # o chmod 0755 /usr/local/bin/checketh.sh 
-# o Add to crontab (usualy `nano /etc/crontab`)
+# o Add to crontab (usualy `nano /etc/crontab` and read below...)
 #
 # Run Every 5 mins - Seems like ever min is over kill unless
 # this is a very common problem.  If once a min change */5 to *
 # once every 2 mins */5 to */2 ... 
 #
+# Ad this line to your crontab line if you want this script running 
+# at every 5 minutes.
 # */5 * * * * root   bash /usr/local/bin/checketh.sh > /dev/null 
 #
 ##################################################################
@@ -37,6 +39,7 @@ utp='eth0'
 # Log file
 logfile='/var/log/routereth.log'
 ##################################################################
+
 ### Check if log files are in place.
 if [ ! -e $logfile ]; then
 touch $logfile
@@ -52,16 +55,13 @@ if [ -e $lockfile ]; then
     pid=`cat $lockfile`
     if kill -0 &>1 > /dev/null $pid; then
         # Still Valid... lets let it be...
-        #echo "Process still running, Lockfile valid" # Remove for testing
         exit 1
     else
         # Old Lockfile, Remove it
-        #echo "Old lockfile, Removing Lockfile" # Remove for testing
         rm $lockfile
     fi
 fi
 # If we get here, set a lock file using our current PID#
-#echo "Setting Lockfile" # Remove for testing
 echo $$ > $lockfile
 
 # We can perform check
@@ -73,17 +73,9 @@ else
     ifdown $utp
     sleep 5
     ifup --force $utp
-    ifconfig $utp | grep "inet addr"
 fi
-
-# Remove for testing
-#echo 
-#echo "Current Setting:"
-#ifconfig $utp | grep "inet addr:"
-#echo
  
 # Check is complete, Remove Lock file and exit
-#echo "process is complete, removing lockfile" # Remove for testing
 rm $lockfile
 
 if ! [ "$(ping -I eth0 -c 1 192.168.1.1)" ]; then
